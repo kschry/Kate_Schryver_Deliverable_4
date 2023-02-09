e843c51514a7608eadd4ec0d689d4b13fe0056f0
kates <kates@KATE>
2023-02-09 16:18:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Author: Kate Schryver
+Date: 02/07/2023
+Description: The first 25 numbers of the Fibonacci sequence
+*/
+using System;
+namespace Kate_Schryver_Deliverable_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25] ;
+            int a = 2;
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+            foreach (int element in fibonacci)
+            {
+                fibonacci[0] = fibonacci[a - 1] + fibonacci[a - 2];
+                Console.WriteLine ("Fibonacci number " + element + "= " + fibonacci[element]);
+            }
+
+        }
+    }
+}
+
+          
